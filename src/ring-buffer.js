f70e887d1db0ca8e3a1689003cc00a020e42bca9
Paul O'Keeffe <paul.okeeffe@columina.com>
2015-02-22 18:12:37 +1000

    Added ring buffer implementation.

---
+++ src/ring-buffer.js
@@ -0,0 +1,14 @@
+var RingBuffer = function(length) {
+  var pointer = 0;
+  var buffer = [];
+
+  return {
+    get: function(key) {
+      return buffer[key];
+    },
+    push: function(item) {
+      buffer[pointer] = item;
+      pointer = (pointer + 1) % length;
+    }
+  };
+};
