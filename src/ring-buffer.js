d8ef5d96082ea0034eb11baea4751d7f36a266a5
Paul O'Keeffe <paul.okeeffe@columina.com>
2015-02-22 20:20:56 +1000

    Fixed outward view of ordering of elements in ring buffer.

--- src/ring-buffer.js
+++ src/ring-buffer.js
@@ -4,14 +4,20 @@ var RingBuffer = function(length) {
 
   return {
     get: function(key) {
-      return buffer[key];
+      return buffer[(pointer + key) % length];
     },
     push: function(item) {
       buffer[pointer] = item;
       pointer = (pointer + 1) % length;
     },
     map: function(callback) {
-      return buffer.map(callback);
+      var result = new RingBuffer(length);
+      var i = 0;
+      do {
+        result.push(callback.call(buffer.get(i));
+        i++;
+      } while (i < length);
+      return result;
     }
   };
 };
